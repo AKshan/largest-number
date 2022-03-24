71dd2f33ad8fca7f159dfdf62fb34b9c42630cbf
Akshan <akshangupta78@gmail.com>
2022-03-24 18:02:12 +0000

    refactoring

--- src/app/form-control/form-control.component.ts
+++ src/app/form-control/form-control.component.ts
@@ -7,9 +7,6 @@ import {Component, OnInit} from '@angular/core';
 })
 export class FormControlComponent implements OnInit {
   mathForm: any;
-  number1!: number;
-  number2!: number;
-  number3!: number;
   output = ''
   validation = ''
 
@@ -20,9 +17,9 @@ export class FormControlComponent implements OnInit {
 
   }
 
-  calculate(n1: number, n2: number, n3: number) {
-    this.output ='';
-    this.validation ='';
+  /*calculate(n1: number, n2: number, n3: number) {
+    this.output = '';
+    this.validation = '';
     n1 = Math.pow(n1, 2);
     n2 = Math.pow(n2, 2);
     n3 = Math.pow(n3, 2);
@@ -35,14 +32,26 @@ export class FormControlComponent implements OnInit {
         this.output = "The number with largest square is Number 2 and value is= " + `${n2}`;
       } else if (n3 > n2 && n3 > n1) {
         this.output = "The number with largest square is Number 3 and value is= " + `${n3}`;
-      } else if(n1 === n2 || n1 === n3 || n2 === n3){
+      } else if (n1 === n2 || n1 === n3 || n2 === n3) {
         this.validation = "The number is equal";
       }
 
     } else {
       this.validation = 'At least one invalid number was entered.';
     }
-  }
+  }*/
 
+  /*test(n = []) {
+    n.reduce((t, v) => {
+      (t * t > v * v) ? return t :  v;
+    })
+  }*/
+
+  calculate(n:any){
+    const arr = Object.values(n);
+    //const result = arr.map((item:any) => item * item).reduce((t:any, v:any) => (t * t > v * v) ? t: v);
+    const result = arr.map((item:any) => item).reduce((t:any, v:any) => (t * t > v * v) ? t: v);
+    (isNaN(result)) ? console.log('At least one invalid number was entered') : console.log(`The number with the largest square is ${result}`)
+  }
 
 }
